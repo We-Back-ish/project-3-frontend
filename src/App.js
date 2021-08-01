5375e5ccae23add0169be6028d2d479e96723e19
imupallnighttogitsome <artishard.palmer@gmail.com>
2021-08-01 13:55:16 -0500

    this may work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
+
 import './App.css';
-import EventForm from './components/events/EventForm.jsx';
-import { Route, Link } from "react-router-dom";
+import { Route, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+import Login from './components/userinfo/UserLogin';
+import SignUp from './components/userinfo/UserSignup';
+import Home from './components/userinfo/Home';
+import Profile from './components/userinfo/UserProfile';
+import AuthService from "./components/userinfo/services/auth.service";
+
 
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
+
+  const logOut = () => {
+    AuthService.logout();
+  };
+
   return (
     <div className="App">
-      <Link to='/events/add'>
-        <h1>click</h1>
-      </Link>
-      <main>
-        <Route path='/events/add'
-        component={EventForm}/>
-        </main>    
-        </div>
+       
+       {currentUser ? (
+                  <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/user/profile"} className="nav-link">
+                {currentUser.username}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a href="/login" className="nav-link" onClick={logOut}>
+                LogOut
+              </a>
+            </li>
+          </div>
+        ) : (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Sign Up
+              </Link>
+            </li>
+            <main>
+            <Route exact path={["/", "/home"]} component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={SignUp} />
+          <Route exact path="/profile" component={Profile} />  
+            </main>
+          </div>
+        )}
+      
+    </div>
   );
 }
 
